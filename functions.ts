edc065b25f8ff982e982af8b0abf05254f292da7
Extraordinary Studio <56777627+jyc-jyc@users.noreply.github.com>
2022-06-22 10:31:36 +0800

    v1.0.0.0 Update

--- functions.ts
+++ functions.ts
@@ -6,22 +6,30 @@ class FreeHtmlParser{
     parse(_compress:boolean = false):string{
         const minify = require('html-minifier').minify;
         let _style:string = "";
-        let _result:string = "";
+        let _result:string = this._content;
         let _temp:RegExpMatchArray | null;
-        _temp = this._content.match(/(?<=(<bgcol>)).*?(?=(<\/bgcol>))/gi);
+        _temp = _result.match(/(?<=(<bgcol>)).*?(?=(<\/bgcol>))/gi);
         if(_temp){
             _style += `body{background-color:${_temp[_temp.length - 1]}}`;
         }
-        _result = this._content
+        _temp = _result.match(/(?<=(<bgimg>)).*?(?=(<\/bgimg>))/gi);
+        if(_temp){
+            _style += `body{background-image:url(${_temp[_temp.length - 1]});background-repeat:no-repeat;background-size:cover;background-position:center}`;
+        }
+        if(_result.match(/(<html5>)|(<!html>)/gi)){
+            _result = _result
+                .replace(/(<center>)/gi, `<div style="display:flex;align-items:flex-end;justify-content:center">`)
+                .replace(/(<\/center>)/gi, `</div>`);
+        }
+        _result = _result
             .replace(/(<html5>)|(<!html>)/gi, `<!DOCTYPE html>`)
             .replace(/(<html4>)/gi, `<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">`)
             .replace(/(<hcode>)/gi, `<meta charset="`)
             .replace(/(<icon>)/gi, `<link rel="icon" type="image/x-icon" href="`)
             .replace(/(<info>)/gi, `<meta name="description" content="`)
             .replace(/(<key>)/gi, `<meta name="keywords" content="`)
+            .replace(/(<self-adaption\s?\/?>)/gi, `<meta name="viewport" content="width=device-width">`)
             .replace(/(<\/info>)|(<\/icon>)|(<\/hcode>)|(<\/key>)/gi, `">`)
-            .replace(/(<center>)/gi, `<div style="text-align:center">`)
-            .replace(/(<\/center>)/gi, `</div>`)
             .replace(/\s*(<bgcol>|<bgimg>).*?(<\/bgcol>|<\/bgimg>)/gi, '');
         if(_style){
             _result += `\n<style>${_style}</style>`
--- main.ts
+++ main.ts
@@ -4,11 +4,11 @@ const args:string[] = process.argv.slice();
 let data:string = "";
 switch(args[2]){
     case '--about' || '-about':
-        console.log("Nodejs Free HTML Parser [Version 1.0.0]\nCopyright (c) 2022 \x1b[32mGrass Development Team.\x1b[0m\n");
-        console.log("FreeHTML is a simplified HTML. You can use it to write web page more easily. Free HTML was first mentioned and made by Aroton Studio.\nBasic FreeHTML Docs: https://docs.aroton.top/docs/freehtml-help.txt\nDocs of Nodejs FreeHTML Parser are still writing.");
+        console.log("Grass Free HTML Parser [Version 1.0.0]\nCopyright (c) 2022 \x1b[32mGrass Development Team.\x1b[0m\n");
+        console.log("FreeHTML is a simplified HTML. You can use it to write web page more easily. Free HTML was first mentioned and made by Aroton Studio.\nBasic FreeHTML Docs: https://docs.aroton.top/docs/freehtml-help.txt\nDocs of Grass FreeHTML Parser are still writing.");
         break;
     case '--help' || '-help' || '-?' || '--?':
-        console.log("    --about About NFP\n    [FHT File Path] [Out Path] Build HTML file\n");
+        console.log("    --about About GFP\n    [FHT File Path] [Out Path] Build HTML file\n");
         break;
     case undefined:
         fs.access(`./fhtconfig.json`, fs.constants.F_OK, (err:string) =>{
